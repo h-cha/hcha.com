2dc78c29069b7eba67bdbcbab7bf80db18c60d06
h-cha <cocoa0331oreo@gmail.com>
2020-11-09 16:27:39 +0900

    20201109

--- hello/views.py
+++ hello/views.py
@@ -4,38 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    '''
-    params = {
-        'goto_work1' : 'work1',
-        'goto_work2' : 'work2',
-        'goto_work3' : 'work3',
-        'goto_work4' : 'work4',
-    }
-    '''
     return render(request, 'hello/index.html')
-
-'''
-def work1(request):
-    params = {
-        
-    }
-    return render(request, 'hello/works.html',params)
-
-def work2(request):
-    params = {
-        
-    }
-    return render(request, 'hello/works.html',params)
-
-def work3(request):
-    params = {
-        
-    }
-    return render(request, 'hello/works.html',params)
-
-def work4(request):
-    params = {
-        
-    }
-    return render(request, 'hello/works.html',params)
-'''
