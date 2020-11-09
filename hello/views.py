4d6e986ba2aa9c49d50a3f00c378e9508c9bb055
h-cha <cocoa0331oreo@gmail.com>
2020-11-09 16:44:02 +0900

    20201109

--- hello/views.py
+++ hello/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    return render(request, 'hello/index.html')
+
+def test(request):
     return render(request, 'hello/test.html')
