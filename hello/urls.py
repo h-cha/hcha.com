6de10a32241d8ded529290c1b1cff2815265d576
h-cha <cocoa0331oreo@gmail.com>
2020-11-09 22:38:59 +0900

    20201109

--- hello/urls.py
+++ hello/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'hello'
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('work1', views.work1, name='work1'),
+    path('work1', views.work1, name='work1'),
     #path('work2', views.work1, name='work2'),
     #path('work3', views.work1, name='work3'),
     #path('work4', views.work1, name='work4'),
