a9421e7469bd9818ce052683067dbc6ae0d0721d
h-cha <cocoa0331oreo@gmail.com>
2020-11-10 11:33:22 +0900

    20201109

--- hello/urls.py
+++ hello/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'hello'
+#app_name = 'hello'
 urlpatterns = [
     path('', views.index, name='index'),
     path('test', views.test, name='test'),
     path('work1', views.work1, name='work1'),
-    #path('work2', views.work1, name='work2'),
-    #path('work3', views.work1, name='work3'),
-    #path('work4', views.work1, name='work4'),
+    path('work2', views.work1, name='work2'),
+    path('work3', views.work1, name='work3'),
+    path('work4', views.work1, name='work4'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -8,32 +8,43 @@ from django.http import HttpResponse
  #   return render(request, 'hello/index.html')
 
 def test(request):
-    return render(request, 'hello/test.html')
+    params = {
+        'goto' : 'work1',
+    }
+    return render(request, 'hello/test.html',params)
 
 def index(request):
     params = {
         'title' : 'hcha',
-        'goto' : 'work1',
-      #  'goto_work2' : 'work2',
-       # 'goto_work3' : 'work3',
-        #'goto_work4' : 'work4',
+        'goto_work1' : 'work1',
+        'goto_work2' : 'work2',
+        'goto_work3' : 'work3',
+        'goto_work4' : 'work4',
     }
     return render(request, 'hello/index.html', params)
 
 
 def work1(request):
-    
+    params = {
+        'goto' : 'index',
+    }
 
-    return render(request, 'hello/test.html')
+    return render(request, 'hello/works.html',params)
 
 def work2(request):
-    
-    return render(request, 'hello/works.html')
+    params = {
+        'goto' : 'index',
+    }
+    return render(request, 'hello/works.html', params)
 
 def work3(request):
-    
-    return render(request, 'hello/works.html')
+    params = {
+        'goto' : 'index',
+    }
+    return render(request, 'hello/works.html',params)
 
 def work4(request):
-    
-    return render(request, 'hello/works.html')
+    params = {
+        'goto' : 'index',
+    }
+    return render(request, 'hello/works.html', params)
