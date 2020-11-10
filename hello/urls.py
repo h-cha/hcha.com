b7f6b468296f5ef1dbdc17fff87144437f1c5639
h-cha <cocoa0331oreo@gmail.com>
2020-11-10 13:28:16 +0900

    20201109

--- hello/urls.py
+++ hello/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test', views.test, name='test'),
     path('work1', views.work1, name='work1'),
-    path('work2', views.work1, name='work2'),
-    path('work3', views.work1, name='work3'),
-    path('work4', views.work1, name='work4'),
+    path('work2', views.work2, name='work2'),
+    path('work3', views.work3, name='work3'),
+    path('work4', views.work4, name='work4'),
 ]
