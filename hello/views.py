09da293c095aeee07d8254fce0f366354a497b26
h-cha <cocoa0331oreo@gmail.com>
2020-11-10 12:36:09 +0900

    20201109

--- hello/views.py
+++ hello/views.py
@@ -27,6 +27,9 @@ def index(request):
 def work1(request):
     params = {
         'goto' : 'index',
+        'work_name' : 'リアルタイムオンラインチャット',
+        'text' : 'ユーザー認証付きのチャットアプリ<br>django_channelsというライブラリを使ってWebSocket通信を行いました。<br>もともとはワードウルフゲームをする予定でしたが、難しくて断念',
+        'skill' : '#Django #SQLite #html #css #javascript',
     }
 
     return render(request, 'hello/works.html',params)
@@ -34,17 +37,26 @@ def work1(request):
 def work2(request):
     params = {
         'goto' : 'index',
+        'work_name' : '中学生の数学のWEB教科書',
+        'text' : '動画解説を見た後問題演習を行うアプリ<br>中学校に行き、中学生の数学の学習補助を行いました。<br>そこで中学生がつまづいていた課題をサポートするWEBアプリを２人で作成しました。<br>動画解説はWEBの教科書にしかできない利点として評価されました。',
+        'skill' : '#Django #SQLite #html #css',
     }
     return render(request, 'hello/works.html', params)
 
 def work3(request):
     params = {
         'goto' : 'index',
+        'work_name' : '地域の高齢化がわかるアプリ',
+        'text' : '郵便番号を入力するとその地域の世代別人口の割合が円グラフで表示されるアプリを作りました。',
+        'skill' : '#java #postgreSQL #html #css #javascript',
     }
     return render(request, 'hello/works.html',params)
 
 def work4(request):
     params = {
         'goto' : 'index',
+        'work_name' : '食材残さないよ太郎',
+        'text' : '現在作成中です',
+        'skill' : '#django #SQLite #html #css',
     }
     return render(request, 'hello/works.html', params)
